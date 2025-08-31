a25cc3d9dea0744604d52642920c366c74a4e924
DJedaj <mihail.sizonenko@mail.ru>
2025-08-31 22:10:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
-    
+
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///parking.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
     db.init_app(app)
-    
+
     from routes import main_bp
     app.register_blueprint(main_bp)
 
     return app
+
 
 if __name__ == '__main__':
     app = create_app()
